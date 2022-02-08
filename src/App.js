4d1d18e46893e938884db6089b22121511f6ac80
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-08 11:28:16 +0100

    working on React and Firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
@@ -19,12 +20,10 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
     </>
   );
 }
 
 export default App;
-
-// JavaScript FCC interview
-// JavaScript CodeAcademy interview
