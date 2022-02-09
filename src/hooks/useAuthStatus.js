06f7d451f737f682742a9a4556de17a20522a3f4
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-09 10:12:33 +0100

    working on react and firebase

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,38 @@
+import { useEffect, useState, useRef } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setCheckingStatus(false);
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  return { loggedIn, checkingStatus };
+};
+
+export default useAuthStatus;
+
+// useEffect(() => {
+//   const unsub = onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//       setLoggedIn(true);
+//     }
+//     setIsPending(false);
+//   });
+//   return () => unsub();
+// }, []);
